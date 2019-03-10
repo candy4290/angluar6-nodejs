0b4537793d80521467c399a80f3278e4ebbf7768
陈小祥 <2459402883@qq.com>
2019-03-10 14:32:04 +0800

    ueditor百度富文本编辑器

--- projects/cxx-web/src/app/pages/editors/umeditor/umeditor.component.ts
+++ projects/cxx-web/src/app/pages/editors/umeditor/umeditor.component.ts
@@ -1,27 +1,74 @@
-import { Component, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterViewInit, ViewChild } from '@angular/core';
 import { SafeHtml, DomSanitizer } from '@angular/platform-browser';
+import { Ng2Ueditor } from 'ng2-ueditor';
 
 @Component({
   selector: 'app-umeditor',
   templateUrl: './umeditor.component.html',
   styleUrls: ['./umeditor.component.scss']
 })
-export class UmeditorComponent implements OnInit, OnDestroy, AfterViewInit {
-  full_source: any;
-  config = {
-    toolbar: ['undo redo | bold italic underline'],
-    initialFrameHeight: 100
+export class UmeditorComponent {
+  @ViewChild('ueditor') ueditor: Ng2Ueditor;
+
+  content1: any;
+
+  setting = {
+    // 这里可以选择自己需要的工具按钮名称,此处仅选择如下五个
+    toolbars: [[
+      'fullscreen', 'source', '|', 'undo', 'redo', '|',
+      'bold', 'italic', 'underline', 'fontborder',
+      'strikethrough', 'removeformat', 'formatmatch', 'autotypeset',
+      'blockquote', 'pasteplain', '|', 'forecolor', 'backcolor', 'insertorderedlist', 'insertunorderedlist', 'selectall', 'cleardoc', '|',
+      // 'rowspacingtop', 'rowspacingbottom', 'lineheight', '|',
+      'customstyle', 'paragraph', 'fontfamily', 'fontsize', '|',
+      // 'directionalityltr', 'directionalityrtl', 'indent', '|',
+      'justifyleft', 'justifycenter', 'justifyright', 'justifyjustify', '|',
+      // 'touppercase', 'tolowercase', '|',
+      // 'link', 'unlink', 'anchor', '|',
+      // 'imagenone', 'imageleft', 'imageright', 'imagecenter', '|',
+      'simpleupload', 'insertimage',
+      // 'emotion', 'scrawl',
+      // 'insertvideo', 'music',
+      // 'attachment',
+      // 'map', 'gmap',
+      // 'insertframe', 'insertcode', 'webapp', 'pagebreak', 'template',
+      'background', '|',
+      // 'horizontal', 'date', 'time', 'spechars', 'snapscreen', 'wordimage', '|',
+      'inserttable', 'deletetable', 'insertparagraphbeforetable',
+      'insertrow', 'deleterow', 'insertcol', 'deletecol', 'mergecells',
+      'mergeright', 'mergedown', 'splittocells', 'splittorows', 'splittocols',
+      // 'charts',
+      '|',
+      // 'print',
+      'preview',
+      // 'searchreplace', 'drafts', 'help'
+  ]],
+    // focus时自动清空初始化时的内容
+    autoClearinitialContent: true,
+    // 关闭字数统计
+    wordCount: false,
+    // 关闭elementPath
+    elementPathEnabled: false,
+    // 默认的编辑区域高度
+    initialFrameHeight: 300,
+
+    // enableAutoSave: false,
+    // autoSyncData: false,
+    // 更多其他参数，请参考ueditor.config.js中的配置项
+    serverUrl: '/server/ueditor/controller.php'
   };
-  constructor(private sanitizer: DomSanitizer) {
-  }
-  ngOnInit(): void {
 
-  }
-
-  ngOnDestroy(): void {
+  onValueChange(value: string) {
+    console.log('---编辑器内容发生改变---');
+    console.log('编辑器内容：' + value);
   }
 
-  ngAfterViewInit(): void {
+  printInfo() {
+    console.log('---打印信息---');
+    console.log('编辑器内容：' + this.ueditor.getContent());
+    console.log('纯文本内容，保留段落格式：' + this.ueditor.getPlainTxt());
+    console.log('纯文本内容，没有段落格式：' + this.ueditor.getContentTxt());
+    console.log('是否有内容：' + this.ueditor.hasContents());
   }
 
 }
--- projects/cxx-web/src/app/pages/editors/umeditor/umeditor.module.ts
+++ projects/cxx-web/src/app/pages/editors/umeditor/umeditor.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { UmeditorComponent } from './umeditor.component';
 import { UmeditorRoutingModule } from './umeditor-routing.module';
 import { SharedModule } from '../../../shared/shared.module';
-
+import { Ng2UeditorModule } from 'ng2-ueditor';
 @NgModule({
   imports: [
     SharedModule,
-    UmeditorRoutingModule
+    UmeditorRoutingModule,
+    Ng2UeditorModule
   ],
   declarations: [UmeditorComponent]
 })
